c233c0d11661851fb0056a90dcd25d2bb8548585
Eduardo Andrade Ferreira <135661746+eduardoanfer@users.noreply.github.com>
2025-04-25 17:36:12 -0300

    Initial commit

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Models
+{
+    [Table("Categoria")] // gerar uma tabela com nome categoria com Schema
+    public class Category
+    {
+        [Key]
+        [Column]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Esse Campo é Obrigatório")]
+        [MaxLength(60, ErrorMessage = "Esse Campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Esse Campo deve conter entre 3 e 60 caracteres")]
+
+        public string Title { get; set; }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models;
+//using System.Data.Anotations
+public class Product
+{
+    [Key]
+
+    public int ProductId { get; set; }
+    [Required(ErrorMessage = "o nome deve ser preenchido")]
+    [MaxLength(60,ErrorMessage = "deve conter no maximo 3 a 60 caracteres")]
+    [MinLength(30,ErrorMessage = "deve conter no maximo 3 caracteres")]
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
+    public string ProductImage { get; set; }
+
+}
