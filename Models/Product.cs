b47b0cac78bd80d9b6fcdc02dd152aeaa91aa63d
Eduardo Andrade Ferreira <135661746+eduardoanfer@users.noreply.github.com>
2025-04-25 17:57:34 -0300

    feat/01
    Criando Category, Product e User

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,13 +6,19 @@ public class Product
 {
     [Key]
 
-    public int ProductId { get; set; }
+    public int Id { get; set; }
     [Required(ErrorMessage = "o nome deve ser preenchido")]
     [MaxLength(60,ErrorMessage = "deve conter no maximo 3 a 60 caracteres")]
     [MinLength(30,ErrorMessage = "deve conter no maximo 3 caracteres")]
     public string ProductName { get; set; }
+    [MaxLength(1024,ErrorMessage = "Esse Campo deve ser Preenchido")]
     public string ProductDescription { get; set; }
+    [MaxLength(ErrorMessage = "Esse Campo é obrigatorio")]
+    [Range(1,int.MaxValue, ErrorMessage = "Categoria invalida")]
     public decimal ProductPrice { get; set; }
-    public string ProductImage { get; set; }
+    [MaxLength(ErrorMessage = "Esse Campo é obrigatorio")]
+    [Range(1,int.MaxValue, ErrorMessage = "Categoria invalida")]
+    public int CategoryId { get; set; } // so a referência da categoria
+    public Category Category { get; set; } // saber mais detalhes da categoria informaçoes do objeto dentro do outro. 
 
 }
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "o nome deve ser preenchido")]
+    [MaxLength(20,ErrorMessage = "deve conter no maximo 3 a 60 caracteres")]
+    [MinLength(3,ErrorMessage = "deve conter no maximo 3 a 60 caracteres")]
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
